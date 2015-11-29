a0179bbde33af03f06a4aeec591ae41a6a4f82ab
Disco Patrick <discopatrick@me.com>
2015-11-29 20:06:27 +0000

    Actually quite pretty

--- app/main.js
+++ app/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-console.log('Hello, world!')
+const scrollbarMargin = 8;
+const width = window.innerWidth - scrollbarMargin; 
+const height = window.innerHeight - scrollbarMargin;
+
+window.init = function init() {
+  const canvas = document.querySelector('canvas');
+  canvas.width = width; 
+  canvas.height = height;
+
+  const ctx = canvas.getContext('2d');
+  const image = new Image();
+  image.src = 'landscape.jpg';
+  image.onload = () => run(ctx, image)
+}
+
+function run(ctx, image) {
+  // top left
+  ctx.drawImage(image, 0, 0, width/2, height/2);
+  // top right
+  ctx.translate(width, 0);
+  ctx.scale(-1, 1);
+  ctx.drawImage(image, 0, 0, width/2, height/2);
+  // bottom right
+  ctx.translate(0, height);
+  ctx.scale(1, -1);
+  ctx.drawImage(image, 0, 0, width/2, height/2);
+  // bottom left
+  ctx.translate(width, 0);
+  ctx.scale(-1, 1);
+  ctx.drawImage(image, 0, 0, width/2, height/2);
+}
